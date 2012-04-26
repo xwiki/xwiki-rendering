80a8e34e9ff46368eb2e40b9a4bae82b6ab912be
Vincent Massol <vincent@massol.net>
2012-04-26 22:54:51 +0200

    Revert "XRENDERING-187: Implement a Rendering Compatibility Test Suite"
    This reverts commit 9af7ca9861e67bfdf41a0f0fa59a4f547c9e04a2.

--- xwiki-rendering-test/src/test/java/org/xwiki/rendering/test/cts/TestDataGeneratorTest.java
+++ xwiki-rendering-test/src/test/java/org/xwiki/rendering/test/cts/TestDataGeneratorTest.java
@@ -26,12 +26,6 @@ import org.junit.Test;
 
 import junit.framework.Assert;
 
-/**
- * Unit tests for {@link TestDataGenerator}.
- *
- * @version $Id$
- * @since 4.1M1
- */
 public class TestDataGeneratorTest
 {
     @Test
