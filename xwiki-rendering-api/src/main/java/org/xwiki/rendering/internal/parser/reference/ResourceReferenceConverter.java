6635720ca1c7520797e49dd1e617129026b6cf65
Vincent Massol <vincent@massol.net>
2012-08-27 18:03:55 +0200

    [Misc] Move ResourceReference Converter to a better location (it doesn't have anything to do with Macros)

--- xwiki-rendering-api/src/main/java/org/xwiki/rendering/internal/parser/reference/ResourceReferenceConverter.java
+++ xwiki-rendering-api/src/main/java/org/xwiki/rendering/internal/parser/reference/ResourceReferenceConverter.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.rendering.internal.macro;
+package org.xwiki.rendering.internal.parser.reference;
 
 import javax.inject.Inject;
 import javax.inject.Named;
