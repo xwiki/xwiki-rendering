037c9daf8dbe2ada3e433a54d1bd186a11dbc388
Jakub Jirutka <jakub@jirutka.cz>
2013-06-18 11:19:03 +0200

    XRENDERING-297: Move all specific tests for Markdown to the proper subfolder

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-markdown/src/test/java/org/xwiki/rendering/internal/markdown/MarkdownSpecificTest.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-markdown/src/test/java/org/xwiki/rendering/internal/markdown/MarkdownSpecificTest.java
@@ -23,13 +23,14 @@ import org.junit.runner.RunWith;
 import org.xwiki.rendering.test.integration.RenderingTestSuite;
 
 /**
- * Run all tests found in {@code *.test} files located in the classpath. These {@code *.test} files must follow the
- * conventions described in {@link org.xwiki.rendering.test.integration.TestDataParser}.
+ * Run all specific tests found in {@code *.test} files located in the classpath. These {@code *.test} files must follow
+ * the conventions described in {@link org.xwiki.rendering.test.integration.TestDataParser}.
  *
  * @version $Id$
  * @since 4.1M1
  */
 @RunWith(RenderingTestSuite.class)
-public class IntegrationTests
+@RenderingTestSuite.Scope("markdown10.specific")
+public class MarkdownSpecificTest
 {
 }
