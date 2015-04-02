c3a0a5410724f2dc76317919955ccc0342d22cd8
Denis Gervalle <dgl@softec.lu>
2015-04-02 12:07:17 +0200

    XWIKI-11791: Failing to paste some content in the WYSIWYG editor
    - Fixup inconsistancies in xwikimodel, that was semi-handling unknown tags, causing stacks mismatching.
    - Improved support of HTML5 block tags (aside, section, article, main, nav, details, summary, figure, figcaption, header, footer)
    
    (cherry-picked and adapted from 8a7723ac6e9b3fee5e240358807edef09c864db6 and 8ac9420ea6c8c97618e9be39f07c5ca5fb3949b6)

--- xwiki-rendering-wikimodel/src/test/java/org/xwiki/rendering/wikimodel/test/xhtml/XHtmlParserTest.java
+++ xwiki-rendering-wikimodel/src/test/java/org/xwiki/rendering/wikimodel/test/xhtml/XHtmlParserTest.java
@@ -597,4 +597,112 @@ public class XHtmlParserTest extends AbstractWikiParserTest
             "<html><p><a id=\"foo\">bar</a></p></html>",
             "<p></p>");
     }
+
+    /**
+     * Test that unsupported tags, like HTML5 ones, are simply ignored
+     * @throws WikiParserException
+     */
+    public void testUnsupportedTags() throws WikiParserException
+    {
+        test("<html>This is <unsupported>tags</unsupported> ignored</html>",
+            "<p>This is tags ignored</p>");
+
+        test("<html><dl><dt>term</dt><dd>definition<unsupported><dl><dt>term</dt><dd>definition</dd></dl></unsupported></dd></dl></html>",
+            "<dl>\n"
+                + "  <dt>term</dt>\n"
+                + "  <dd>definition<dl>\n"
+                + "  <dt>term</dt>\n"
+                + "  <dd>definition</dd>\n"
+                + "</dl>\n"
+                + "</dd>\n"
+                + "</dl>");
+
+        test(
+            "<html><ul><li>a<unsupported><ul><li>b</li></ul></unsupported></li></ul></html>",
+            ""
+                + "<ul>\n"
+                + "  <li>a<ul>\n"
+                + "  <li>b</li>\n"
+                + "</ul>\n"
+                + "</li>\n"
+                + "</ul>");
+
+        test(
+            "<html><blockquote>line1<unsupported><blockquote>line2<blockquote>line3</blockquote>"
+                + "line4</blockquote></unsupported></blockquote></html>",
+            "<blockquote>\nline1<blockquote>\nline2<blockquote>\nline3\n</blockquote>"
+                + "\n\nline4\n</blockquote>\n\n</blockquote>");
+    }
+
+    /**
+     * Test malformed constructs created by the HTML cleaner not supporting HTML5
+     * @throws WikiParserException
+     */
+    public void testUnsupportedHTML5Tags() throws WikiParserException
+    {
+        test(
+            "<html><p><section><p>test</p></section></p></html>",
+            "<p></p>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>test</p>\n"
+                + "</div>");
+        test(
+            "<html><p><header><h1>test</h1></header></p></html>",
+            "<p></p>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<h1>test</h1>\n"
+                + "</div>");
+
+        test(
+            "<html><p><header><h1>test</h1></header></p></html>",
+            "<p></p>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<h1>test</h1>\n"
+                + "</div>");
+    }
+    
+    /**
+     * Test basic HTML5 support
+     * @throws WikiParserException
+     */
+    public void testBasicHTML5Tags() throws WikiParserException
+    {
+        test(
+            "<html><header>Header<nav>Navigation</nav></header><main><article><section><h1>section 1</h1><summary>Summary 1</summary><details>Text 1<figure><img src='target'/><figcaption>caption</figcaption></figure></details><aside>Aside</aside></section><section><h1>section 2</h1><p>text 2</p></section></article></main><footer>Footer</footer></html>",
+            "<div class='wikimodel-document'>\n"
+                + "<p>Header</p>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>Navigation</p>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<h1>section 1</h1>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>Summary 1</p>\n"
+                + "</div>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>Text 1</p>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p><img src='target'/></p>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>caption</p>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>Aside</p>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<h1>section 2</h1>\n"
+                + "<p>text 2</p>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "<div class='wikimodel-document'>\n"
+                + "<p>Footer</p>\n"
+                + "</div>");
+    }
 }
