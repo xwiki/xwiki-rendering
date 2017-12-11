df846e4dc4baa1357818b0a4845960632dc5e121
Piyush Gupta <piyush16066@iiitd.ac.in>
2017-12-11 13:08:34 +0530

    [codestyle] Reduce the number of returns
    Reduce the number of returns of this method 4, down to the maximum allowed 3

--- xwiki-rendering-macros/xwiki-rendering-macro-ctsreport/src/main/java/org/xwiki/rendering/internal/macro/ctsreport/Result.java
+++ xwiki-rendering-macros/xwiki-rendering-macro-ctsreport/src/main/java/org/xwiki/rendering/internal/macro/ctsreport/Result.java
@@ -59,14 +59,11 @@ public class Result
     @Override
     public boolean equals(Object object)
     {
-        if (object == null) {
+        if (object == null || object.getClass() != getClass()) {
             return false;
         }
         if (object == this) {
             return true;
-        }
-        if (object.getClass() != getClass()) {
-            return false;
         }
         Result rhs = (Result) object;
         return new EqualsBuilder()
