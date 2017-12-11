c02c4e37906d55035e11899cc9945fbf4c0918da
Arun Pattni <arun.pattni01@gmail.com>
2017-12-11 15:26:16 +0000

    [Codestyle] removed unused variable

--- xwiki-rendering-macros/xwiki-rendering-macro-footnotes/src/main/java/org/xwiki/rendering/internal/macro/footnote/PutFootnotesMacro.java
+++ xwiki-rendering-macros/xwiki-rendering-macro-footnotes/src/main/java/org/xwiki/rendering/internal/macro/footnote/PutFootnotesMacro.java
@@ -175,7 +175,7 @@ public class PutFootnotesMacro extends AbstractMacro<FootnoteMacroParameters>
     private void addFootnoteRef(MacroMarkerBlock footnoteMacro, Block footnoteRef)
     {
         for (ListIterator<Block> it = footnoteMacro.getChildren().listIterator(); it.hasNext();) {
-            Block b = it.next();
+            
             it.remove();
             // b.replace(Collections.<Block> emptyList());
         }
