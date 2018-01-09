07f7e81d1eae33f0ae1245e943d04f8d0fc02cb0
gonzaloUr <gonzalohugouran@gmail.com>
2018-01-08 23:29:53 -0300

    XWikiWikiReference issues

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-xhtml/src/main/java/org/xwiki/rendering/internal/parser/xhtml/wikimodel/XWikiWikiReference.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-xhtml/src/main/java/org/xwiki/rendering/internal/parser/xhtml/wikimodel/XWikiWikiReference.java
@@ -19,6 +19,8 @@
  */
 package org.xwiki.rendering.internal.parser.xhtml.wikimodel;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.xwiki.rendering.listener.reference.ResourceReference;
 import org.xwiki.rendering.wikimodel.WikiParameters;
 import org.xwiki.rendering.wikimodel.WikiReference;
@@ -34,19 +36,19 @@ public class XWikiWikiReference extends WikiReference
 {
     private ResourceReference reference;
 
-    private boolean isFreeStanding;
+    private boolean freeStanding;
 
     public XWikiWikiReference(ResourceReference reference, String label, WikiParameters linkParameters,
-        boolean isFreeStanding)
+        boolean freeStanding)
     {
         super(reference.getReference(), label, linkParameters);
         this.reference = reference;
-        this.isFreeStanding = isFreeStanding;
+        this.freeStanding = freeStanding;
     }
 
     public boolean isFreeStanding()
     {
-        return this.isFreeStanding;
+        return this.freeStanding;
     }
 
     public ResourceReference getReference()
@@ -55,5 +57,29 @@ public class XWikiWikiReference extends WikiReference
     }
 
     // TODO: Ensure there's no sync issue between reference object and getLink() from WikiReference
-    // TODO: implement hashcode, equals, totring
+    // TODO: implement totring
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        EqualsBuilder builder = new EqualsBuilder();
+
+        builder.appendSuper(super.equals(obj));
+        builder.append(reference);
+        builder.append(freeStanding);
+
+        return builder.isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        HashCodeBuilder builder = new HashCodeBuilder();
+
+        builder.appendSuper(super.hashCode());
+        builder.append(reference);
+        builder.append(freeStanding);
+
+        return builder.toHashCode();
+    }
 }
