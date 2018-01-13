cd7b08b69468152169f0ac00498410cbaed183b4
Bluecoder23 <wwwwer23>
2018-01-13 12:29:34 -0500

    checking pullrequest
    testing pullrequest how it looks

--- xwiki-rendering-transformations/xwiki-rendering-transformation-wikiword/src/main/java/org/xwiki/rendering/internal/transformation/wikiword/WikiWordTransformation.java
+++ xwiki-rendering-transformations/xwiki-rendering-transformation-wikiword/src/main/java/org/xwiki/rendering/internal/transformation/wikiword/WikiWordTransformation.java
@@ -1,6 +1,6 @@
 /*
  * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership.
+ * information regarding     copyright ownership.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
