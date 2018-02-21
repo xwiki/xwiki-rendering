cbc280bd113cc0dc9687b2703d2463d8044cee14
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-02-21 10:42:40 +0100

    Revert "[codestyle] Reduce Cyclomatic Complexity of method (#136)" (#137)
    This reverts commit 22204e818c30b9ec9173efe892063f75fcf1cbc4.

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-xwiki20/src/main/java/org/xwiki/rendering/internal/renderer/xwiki20/XWikiSyntaxChainingRenderer.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-xwiki20/src/main/java/org/xwiki/rendering/internal/renderer/xwiki20/XWikiSyntaxChainingRenderer.java
@@ -227,11 +227,16 @@ public class XWikiSyntaxChainingRenderer extends AbstractChainingPrintRenderer i
     public void beginFormat(Format format, Map<String, String> parameters)
     {
         // If the previous format had parameters and the parameters are different from the current ones then close them
-        if (this.previousFormatParameters != null && !parameters.isEmpty()) {
-            if (!this.previousFormatParameters.equals(parameters)) {
+        if (this.previousFormatParameters != null) {
+            if (parameters.isEmpty()) {
+                // print("(%%)");
+                // this.previousFormatParameters = null;
+            } else if (!this.previousFormatParameters.equals(parameters)) {
+                this.previousFormatParameters = null;
                 printParameters(parameters, false);
+            } else {
+                this.previousFormatParameters = null;
             }
-            this.previousFormatParameters = null;
         } else if (this.previousFormatParameters == null) {
             printParameters(parameters, false);
         }
