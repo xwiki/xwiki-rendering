89e64e81f71090bda64c3f59172341ec0d2031ca
Vincent Massol <vincent@massol.net>
2018-03-29 17:24:27 +0200

    XRENDERING-508: XHTML 1.0 Renderer produces invalid HTML for FigureCaption blocks * Fix TPC

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-xhtml/src/test/java/org/xwiki/rendering/internal/renderer/xhtml/XHTMLChainingRendererTest.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-xhtml/src/test/java/org/xwiki/rendering/internal/renderer/xhtml/XHTMLChainingRendererTest.java
@@ -17,12 +17,11 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.rendering.internal.event;
+package org.xwiki.rendering.internal.renderer.xhtml;
 
 import java.util.Collections;
 
 import org.junit.Test;
-import org.xwiki.rendering.internal.renderer.event.EventsChainingRenderer;
 import org.xwiki.rendering.listener.chaining.ListenerChain;
 import org.xwiki.rendering.renderer.printer.DefaultWikiPrinter;
 import org.xwiki.rendering.renderer.printer.WikiPrinter;
@@ -30,34 +29,28 @@ import org.xwiki.rendering.renderer.printer.WikiPrinter;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Unit tests for {@link EventsChainingRenderer} for methods that cannot be easily tested using the Rendering Test
+ * Unit tests for {@link XHTMLChainingRenderer} for methods that cannot be easily tested using the Rendering Test
  * framework.
  *
  * @version $Id$
  * @since 10.3RC1
  */
-public class EventsChainingRendererTest
+public class XHTMLChainingRendererTest
 {
     /**
      * Those events are hard to test since there's no easy syntax to input them (requires XWiki 2.0+ Syntax with a
      * Macro transformation applied).
      */
     @Test
-    public void outputFigureEvents()
+    public void outputFigureCaptionEvents()
     {
-        EventsChainingRenderer renderer = new EventsChainingRenderer(new ListenerChain());
+        XHTMLChainingRenderer renderer = new XHTMLChainingRenderer(null, null, new ListenerChain());
         WikiPrinter wikiPrinter = new DefaultWikiPrinter();
         renderer.setPrinter(wikiPrinter);
-        renderer.beginFigure(Collections.emptyMap());
         renderer.beginFigureCaption(Collections.emptyMap());
+        renderer.onWord("caption");
         renderer.endFigureCaption(Collections.emptyMap());
-        renderer.endFigure(Collections.emptyMap());
 
-        String expected = "beginFigure\n"
-            + "beginFigureCaption\n"
-            + "endFigureCaption\n"
-            + "endFigure\n";
-
-        assertEquals(expected, wikiPrinter.toString());
+        assertEquals("<p>caption</p>", wikiPrinter.toString());
     }
 }
