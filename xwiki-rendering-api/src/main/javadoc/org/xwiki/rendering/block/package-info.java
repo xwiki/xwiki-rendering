2ebf1a05e681d07030abbe40200e9b7a2b87416e
Vincent Massol <vincent@massol.net>
2018-12-01 18:53:06 +0100

    [Misc] Replace package.html by the more recent and recommended package-info.java (see https://issues.apache.org/jira/browse/MJAVADOC-329?focusedCommentId=16705913&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-16705913)

---
+++ xwiki-rendering-api/src/main/javadoc/org/xwiki/rendering/block/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Represents content elements (Paragraph, Header, List, Styles, etc) as Blocks and a full document content is
+ * represented by a XDOM object.
+ * <p>
+ * For overviews, tutorials, examples, guides, and tool documentation, please see:
+ * <ul>
+ *   <li><a href="http://rendering.xwiki.org">Rendering Documentation</a></li>
+ * </ul>
+ *
+ * @since 1.5M2
+ */
+package org.xwiki.rendering.block;
