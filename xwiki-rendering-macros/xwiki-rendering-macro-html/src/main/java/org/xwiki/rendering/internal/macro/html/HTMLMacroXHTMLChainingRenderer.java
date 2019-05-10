a31664b81db4472b6cf4444069d197440fa25b4f
Simon Urli <simon.urli@xwiki.com>
2019-05-10 10:54:13 +0200

    XRENDERING-563: Handle metadata rendering in HTML macro

--- xwiki-rendering-macros/xwiki-rendering-macro-html/src/main/java/org/xwiki/rendering/internal/macro/html/HTMLMacroXHTMLChainingRenderer.java
+++ xwiki-rendering-macros/xwiki-rendering-macro-html/src/main/java/org/xwiki/rendering/internal/macro/html/HTMLMacroXHTMLChainingRenderer.java
@@ -19,11 +19,14 @@
  */
 package org.xwiki.rendering.internal.macro.html;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.xwiki.rendering.internal.parser.xhtml.wikimodel.XHTMLXWikiGeneratorListener;
 import org.xwiki.rendering.internal.renderer.xhtml.XHTMLChainingRenderer;
 import org.xwiki.rendering.internal.renderer.xhtml.image.XHTMLImageRenderer;
 import org.xwiki.rendering.internal.renderer.xhtml.link.XHTMLLinkRenderer;
+import org.xwiki.rendering.listener.MetaData;
 import org.xwiki.rendering.listener.chaining.BlockStateChainingListener;
 import org.xwiki.rendering.listener.chaining.ListenerChain;
 
@@ -153,5 +156,38 @@ public class HTMLMacroXHTMLChainingRenderer extends XHTMLChainingRenderer
     protected BlockStateChainingListener getBlockState()
     {
         return (BlockStateChainingListener) getListenerChain().getListener(HTMLMacroBlockStateChainingListener.class);
+    }
+
+    /**
+     * @return a span element if we are inside an inline macro. Else a div.
+     */
+    private String getMetadataContainerElement()
+    {
+        if (getBlockState().isInLine()) {
+            return "span";
+        } else {
+            return "div";
+        }
+    }
+
+    @Override
+    public void beginMetaData(MetaData metadata)
+    {
+        Map<String, String> attributes = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Object> metadataPair : metadata.getMetaData().entrySet()) {
+            attributes.put(XHTMLXWikiGeneratorListener.METADATA_ATTRIBUTE_PREFIX + metadataPair.getKey(),
+                metadataPair.getValue().toString());
+        }
+
+        attributes.put("class", XHTMLXWikiGeneratorListener.METADATA_CONTAINER_CLASS);
+
+        this.getXHTMLWikiPrinter().printXMLStartElement(getMetadataContainerElement(), attributes);
+    }
+
+    @Override
+    public void endMetaData(MetaData metadata)
+    {
+        getXHTMLWikiPrinter().printXMLEndElement(getMetadataContainerElement());
     }
 }
