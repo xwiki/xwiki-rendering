e3025253ecd1bbc1e7feea7affd6588e25bbda38
Marius Dumitru Florea <marius@xwiki.com>
2019-06-13 14:12:45 +0300

    XWIKI-8613: Add an autosuggest to any input that requires a document reference in macros * Make it easier to overwrite the Toc macro in platform without duplicating code, in order to be able to associate a DocumentReference picker to the "reference" parameter.

--- xwiki-rendering-macros/xwiki-rendering-macro-toc/src/main/java/org/xwiki/rendering/internal/macro/toc/TocMacro.java
+++ xwiki-rendering-macros/xwiki-rendering-macro-toc/src/main/java/org/xwiki/rendering/internal/macro/toc/TocMacro.java
@@ -19,27 +19,11 @@
  */
 package org.xwiki.rendering.internal.macro.toc;
 
-import java.util.List;
-
-import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
-import org.xwiki.component.phase.InitializationException;
-import org.xwiki.rendering.block.Block;
-import org.xwiki.rendering.block.XDOM;
-import org.xwiki.rendering.listener.reference.DocumentResourceReference;
-import org.xwiki.rendering.listener.reference.ResourceReference;
-import org.xwiki.rendering.macro.AbstractMacro;
-import org.xwiki.rendering.macro.MacroExecutionException;
 import org.xwiki.rendering.macro.toc.TocMacroParameters;
-import org.xwiki.rendering.parser.Parser;
-import org.xwiki.rendering.renderer.reference.link.LinkLabelGenerator;
-import org.xwiki.rendering.transformation.MacroTransformationContext;
-import org.xwiki.rendering.wiki.WikiModel;
-import org.xwiki.rendering.wiki.WikiModelException;
 
 /**
  * Generate a Table Of Contents based on the document sections.
@@ -50,88 +34,13 @@ import org.xwiki.rendering.wiki.WikiModelException;
 @Component
 @Named("toc")
 @Singleton
-public class TocMacro extends AbstractMacro<TocMacroParameters>
+public class TocMacro extends AbstractTocMacro<TocMacroParameters>
 {
-    /**
-     * The description of the macro.
-     */
-    private static final String DESCRIPTION = "Generates a Table Of Contents.";
-
-    private TocTreeBuilder tocTreeBuilder;
-
-    /**
-     * A parser that knows how to parse plain text; this is used to transform link labels into plain text.
-     */
-    @Inject
-    @Named("plain/1.0")
-    private Parser plainTextParser;
-
-    /**
-     * Generate link label.
-     */
-    @Inject
-    private LinkLabelGenerator linkLabelGenerator;
-
-    @Inject
-    private Provider<WikiModel> wikiModelProvider;
-
     /**
      * Create and initialize the descriptor of the macro.
      */
     public TocMacro()
     {
-        super("Table Of Contents", DESCRIPTION, TocMacroParameters.class);
-
-        // Make sure this macro is executed as one of the last macros to be executed since
-        // other macros can generate headers which need to be taken into account by the TOC
-        // macro.
-        setPriority(2000);
-        setDefaultCategory(DEFAULT_CATEGORY_NAVIGATION);
-    }
-
-    @Override
-    public void initialize() throws InitializationException
-    {
-        super.initialize();
-
-        this.tocTreeBuilder = new TocTreeBuilder(new TocBlockFilter(this.plainTextParser, this.linkLabelGenerator));
-    }
-
-    @Override
-    public boolean supportsInlineMode()
-    {
-        return false;
-    }
-
-    @Override
-    public List<Block> execute(TocMacroParameters parameters, String content, MacroTransformationContext context)
-        throws MacroExecutionException
-    {
-        Block rootBlock = null;
-        WikiModel wikiModel = this.wikiModelProvider.get();
-        if (parameters.getReference() != null) {
-            if (wikiModel != null) {
-                // Remote TOC always has a PAGE scope since a LOCAL scope would have no meaning
-                parameters.setScope(TocMacroParameters.Scope.PAGE);
-                // Get the referenced source's XDOM
-                rootBlock = getXDOM(new DocumentResourceReference(parameters.getReference()), wikiModel);
-            } else {
-                throw new MacroExecutionException(
-                    "The \"reference\" parameter can only be used when a WikiModel implementation is available");
-            }
-        }
-
-        TreeParametersBuilder builder = new TreeParametersBuilder();
-        TreeParameters treeParameters = builder.build(rootBlock, parameters, context);
-        return this.tocTreeBuilder.build(treeParameters);
-    }
-
-    private XDOM getXDOM(ResourceReference reference, WikiModel wikiModel) throws MacroExecutionException
-    {
-        try {
-            return wikiModel.getXDOM(reference);
-        } catch (WikiModelException e) {
-            throw new MacroExecutionException(String.format("Failed to get XDOM for [%s]", reference), e);
-        }
+        super(TocMacroParameters.class);
     }
 }
