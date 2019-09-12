91d42c0fc97d88fb29f23ba6ecb5bdde9b9f6898
Thomas Mortagne <thomas.mortagne@gmail.com>
2019-09-12 14:14:18 +0200

    XRENDERING-560: SpaceBlock Equals Every Block Without Children & Parameters
    (cherry picked from commit b9575c321a41d607df8bae79c0b803d69191677f)

---
+++ xwiki-rendering-api/src/test/java/org/xwiki/rendering/block/SpaceBlockTest.java
@@ -0,0 +1,41 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.block;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * Validate {@link SpaceBlock}.
+ *
+ * @version $Id$
+ */
+public class SpaceBlockTest
+{
+    @Test
+    public void testEquals()
+    {
+        assertEquals(new SpaceBlock(), new SpaceBlock());
+        assertNotEquals(new SpaceBlock(), new WordBlock("toto"));
+        assertNotEquals(new WordBlock("toto"), new SpaceBlock());
+    }
+}
