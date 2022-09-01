a07c914ea77010cae9021f171da5b16cacb6e73d
Simon Urli <simon.urli@xwiki.com>
2022-09-01 11:32:25 +0200

    XRENDERING-678: Cannot use HtmlMacro with a syntax installed on a subwiki
      * Use a Provider for the context component manager so that we can rely on the
        context provider CM implemented in xwiki-commons

--- xwiki-rendering-macros/xwiki-rendering-macro-html/src/main/java/org/xwiki/rendering/internal/macro/html/HTMLMacro.java
+++ xwiki-rendering-macros/xwiki-rendering-macro-html/src/main/java/org/xwiki/rendering/internal/macro/html/HTMLMacro.java
@@ -28,6 +28,7 @@ import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
@@ -124,7 +125,8 @@ public class HTMLMacro extends AbstractMacro<HTMLMacroParameters>
     private RenderingContext renderingContext;
 
     @Inject
-    private ComponentManager componentManager;
+    @Named("context")
+    private Provider<ComponentManager> componentManagerProvider;
 
     /**
      * Create and initialize the descriptor of the macro.
@@ -312,7 +314,7 @@ public class HTMLMacro extends AbstractMacro<HTMLMacroParameters>
     {
         String hint = HTMLMacroXHTMLRendererFactory.PREFIX_SYNTAX + targetSyntax.toIdString();
         try {
-            return this.componentManager.getInstance(PrintRendererFactory.class, hint);
+            return this.componentManagerProvider.get().getInstance(PrintRendererFactory.class, hint);
         } catch (ComponentLookupException e) {
             return this.defaultXHTMLRendererFactory;
         }
