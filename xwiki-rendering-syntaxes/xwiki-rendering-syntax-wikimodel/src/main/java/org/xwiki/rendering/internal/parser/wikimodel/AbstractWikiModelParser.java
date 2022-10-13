e2354e515b085431c9b35263154a0e156184b292
Manuel Leduc <manuel.leduc@xwiki.com>
2022-10-13 09:09:38 +0200

    XRENDERING-683: Return a parse exception in case of stackoverflow when parsing with javacc (#226)

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-wikimodel/src/main/java/org/xwiki/rendering/internal/parser/wikimodel/AbstractWikiModelParser.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-wikimodel/src/main/java/org/xwiki/rendering/internal/parser/wikimodel/AbstractWikiModelParser.java
@@ -132,7 +132,9 @@ public abstract class AbstractWikiModelParser implements Parser, WikiModelStream
         IWikiParser parser = createWikiModelParser();
         try {
             parser.parse(source, createXWikiGeneratorListener(listener, idGenerator));
-        } catch (Exception e) {
+        } catch (Exception | StackOverflowError e) {
+            // Stack overflow errors are caught in addition to exceptions because they can be thrown by javacc based
+            // implementations in case of too deeply nested contents (e.g., too many nested groups).   
             throw new ParseException("Failed to parse input source", e);
         }
     }
