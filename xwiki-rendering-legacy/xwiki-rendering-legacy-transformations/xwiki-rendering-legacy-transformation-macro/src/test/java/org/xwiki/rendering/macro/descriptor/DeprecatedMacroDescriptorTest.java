530f842b4b2d8ce3f14d3dce1e2f2deebf1cd995
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-06-19 15:52:17 +0200

    XRENDERING-749: Deprecated MacroDescriptor's getDefaultCategory() is ignored (#308)
    (cherry picked from commit 84bb2686d11d9da17dc445a2d1d3114d71c8246d)

---
+++ xwiki-rendering-legacy/xwiki-rendering-legacy-transformations/xwiki-rendering-legacy-transformation-macro/src/test/java/org/xwiki/rendering/macro/descriptor/DeprecatedMacroDescriptorTest.java
@@ -0,0 +1,89 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.macro.descriptor;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.xwiki.rendering.macro.MacroId;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Validate the retro compatibility of interface {@link MacroDescriptor}.
+ * 
+ * @version $Id$
+ */
+class DeprecatedMacroDescriptorTest
+{
+    private static class TestMacroDescriptor implements MacroDescriptor
+    {
+        @Override
+        public MacroId getId()
+        {
+            return null;
+        }
+
+        @Override
+        public String getName()
+        {
+            return null;
+        }
+
+        @Override
+        public String getDescription()
+        {
+            return null;
+        }
+
+        @Override
+        public Class<?> getParametersBeanClass()
+        {
+            return null;
+        }
+
+        @Override
+        public ContentDescriptor getContentDescriptor()
+        {
+            return null;
+        }
+
+        @Override
+        public Map<String, ParameterDescriptor> getParameterDescriptorMap()
+        {
+            return null;
+        }
+
+        @Override
+        public String getDefaultCategory()
+        {
+            return "deprecatedcategory";
+        }
+    }
+
+    @Test
+    void getDefaultCategory()
+    {
+        TestMacroDescriptor descriptor = new TestMacroDescriptor();
+
+        assertEquals(Set.of("deprecatedcategory"), descriptor.getDefaultCategories());
+    }
+}
