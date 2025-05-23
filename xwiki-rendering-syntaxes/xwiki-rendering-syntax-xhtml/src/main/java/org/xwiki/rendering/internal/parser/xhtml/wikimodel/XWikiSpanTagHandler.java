7550c50bc49558bcf031d146c78d31c8862f9e5b
Michael Hamann <michael.hamann@xwiki.com>
2025-05-23 13:58:06 +0200

    XRENDERING-572: "wikiinternallink" is duplicated at every edit in CKeditor (#346)
    * Add wikiinternallink to the classes to remove.
    * Improve the class removal to work when the class to remove appears
      several times or in combination with other classes to ensure a proper
      cleanup the next time the WYSIWYG editor is used.
    * Add a test to verify that the class is removed as intended.
    * Improve the code style and remove the Checkstyle exclusion.

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-xhtml/src/main/java/org/xwiki/rendering/internal/parser/xhtml/wikimodel/XWikiSpanTagHandler.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-xhtml/src/main/java/org/xwiki/rendering/internal/parser/xhtml/wikimodel/XWikiSpanTagHandler.java
@@ -19,12 +19,19 @@
  */
 package org.xwiki.rendering.internal.parser.xhtml.wikimodel;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.rendering.internal.parser.xhtml.XHTMLParser;
 import org.xwiki.rendering.wikimodel.WikiParameter;
 import org.xwiki.rendering.wikimodel.WikiParameters;
 import org.xwiki.rendering.wikimodel.xhtml.handler.SpanTagHandler;
 import org.xwiki.rendering.wikimodel.xhtml.impl.TagContext;
+
+import static java.util.function.Predicate.not;
 
 /**
  * Handle XWiki span elements (we need to ensure we skip link content when we generate them and also skip the spans for
@@ -35,6 +42,19 @@ import org.xwiki.rendering.wikimodel.xhtml.impl.TagContext;
  */
 public class XWikiSpanTagHandler extends SpanTagHandler implements XWikiWikiModelHandler
 {
+    private static final Set<String> IGNORED_CLASSES = Set.of(
+        "wikilink",
+        "wikiinternallink",
+        "wikicreatelink",
+        "wikiexternallink",
+        "wikiattachmentlink"
+    );
+
+    private static final String CLASS_ATTRIBUTE = "class";
+
+    private static final String WIKI_GENERATED_LINK_CONTENT = "wikigeneratedlinkcontent";
+
+    private static final String XWIKI_RENDERING_ERROR = "xwikirenderingerror";
 
     private XWikiMacroHandler xWikiMacroHandler;
 
@@ -60,22 +80,23 @@ public class XWikiSpanTagHandler extends SpanTagHandler implements XWikiWikiMode
         if (!withNonGeneratedContent) {
             // If we're on a span for unknown links then skip the event for its content.
             // Ex: <a href="..."><span class="wikicreatelinktext">...</span><span class="wikicreatelinkqm">?</span></a>
-            WikiParameter classParam = params.getParameter("class");
+            WikiParameter classParam = params.getParameter(CLASS_ATTRIBUTE);
             if (classParam != null) {
                 String classParamValue = classParam.getValue();
-                if (classParamValue.contains("wikigeneratedlinkcontent"))
+
+                String filteredClassNames = filterIgnoredClasses(classParamValue);
+                if (classParamValue.contains(WIKI_GENERATED_LINK_CONTENT)
+                    || XWIKI_RENDERING_ERROR.equals(classParamValue))
                 {
                     setAccumulateContent(true);
-                } else if ("wikilink".equals(classParamValue)
-                    || "wikicreatelink".equals(classParamValue)
-                    || "wikiexternallink".equals(classParamValue)
-                    || "wikiattachmentlink".equals(classParamValue))
-                {
-                    // Nothing to do
-                } else if ("xwikirenderingerror".equals(classParamValue)) {
-                    setAccumulateContent(true);
-                } else {
+                } else if (filteredClassNames.equals(classParamValue)) {
+                    // Nothing removed means not an ignored class.
                     super.begin(context);
+                } else if (!filteredClassNames.isEmpty()) {
+                    // There was something removed, but the class still isn't empty - most likely due to some bug, an
+                    // intended class was combined with an ignored class.
+                    // Keep the filtered class.
+                    super.begin(updateContext(context, filteredClassNames));
                 }
             } else {
                 super.begin(context);
@@ -89,26 +110,50 @@ public class XWikiSpanTagHandler extends SpanTagHandler implements XWikiWikiMode
         boolean nonGeneratedContent = this.xWikiMacroHandler.handleEnd(context);
 
         if (!nonGeneratedContent) {
-            WikiParameter classParam = context.getParams().getParameter("class");
+            WikiParameter classParam = context.getParams().getParameter(CLASS_ATTRIBUTE);
             if (classParam != null) {
                 String classParamValue = classParam.getValue();
-                if (classParamValue.contains("wikigeneratedlinkcontent"))
+
+                String filteredClassNames = filterIgnoredClasses(classParamValue);
+                if (classParamValue.contains(WIKI_GENERATED_LINK_CONTENT)
+                    || XWIKI_RENDERING_ERROR.equals(classParamValue))
                 {
                     setAccumulateContent(false);
-                } else if ("wikilink".equals(classParamValue)
-                    || "wikicreatelink".equals(classParamValue)
-                    || "wikiexternallink".equals(classParamValue)
-                    || "wikiattachmentlink".equals(classParamValue))
-                {
-                    // Nothing to do
-                } else if ("xwikirenderingerror".equals(classParamValue)) {
-                    setAccumulateContent(false);
-                } else {
+                } else if (filteredClassNames.equals(classParamValue)) {
+                    // Nothing removed means not an ignored class.
                     super.end(context);
+                } else if (!filteredClassNames.isEmpty()) {
+                    // There was something removed, but the class still isn't empty - most likely due to some bug, an
+                    // intended class was combined with an ignored class.
+                    // Keep the filtered class.
+                    super.end(updateContext(context, filteredClassNames));
                 }
             } else {
                 super.end(context);
             }
         }
+    }
+
+    private static TagContext updateContext(TagContext context, String filteredClassNames)
+    {
+        // We cannot update parameters or context, so create a new context.
+        WikiParameters updatedParameters = context.getParams().setParameter(CLASS_ATTRIBUTE, filteredClassNames);
+        return new TagContext(context.getParentContext(), context.getName(), updatedParameters, context.getTagStack());
+    }
+
+    private static String filterIgnoredClasses(String classParamValue)
+    {
+        String[] classes = StringUtils.split(classParamValue);
+        String filteredClassNames;
+        if (Arrays.stream(classes).anyMatch(IGNORED_CLASSES::contains)) {
+            filteredClassNames = Arrays.stream(classes)
+                .filter(not(IGNORED_CLASSES::contains))
+                .collect(Collectors.joining(" "));
+        } else {
+            // Keep the exact original string such that it is easy to check if anything has been removed and to avoid
+            // modifications that could, e.g., lead to non-empty diffs even though nothing has been changed.
+            filteredClassNames = classParamValue;
+        }
+        return filteredClassNames;
     }
 }
