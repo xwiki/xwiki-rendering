b3ddf1740dda122bcf712985798e8682816d108c
Teodor Caras <trrenty@gmail.com>
2026-09-25 10:42:44 +0300

    Toggle list does not convert to block note syntax properly

--- xwiki-rendering-syntaxes/xwiki-rendering-syntax-blocknote/src/main/java/org/xwiki/rendering/internal/parser/blocknote/blocks/ToggleListBlockParser.java
+++ xwiki-rendering-syntaxes/xwiki-rendering-syntax-blocknote/src/main/java/org/xwiki/rendering/internal/parser/blocknote/blocks/ToggleListBlockParser.java
@@ -19,14 +19,26 @@
  */
 package org.xwiki.rendering.internal.parser.blocknote.blocks;
 
+import java.util.Collections;
+import java.util.Deque;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.rendering.internal.parser.blocknote.Context;
+import org.xwiki.rendering.listener.Listener;
+import org.xwiki.rendering.parser.ParseException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import static org.xwiki.rendering.internal.parser.blocknote.blocks.GroupBlockParser.GROUP;
+import static org.xwiki.rendering.internal.parser.blocknote.blocks.NumberedListBlockParser.NUMBERED_LIST_ITEM;
 
 /**
  * Toggle list block parser.
@@ -49,6 +61,37 @@ public class ToggleListBlockParser extends BulletListBlockParser
      * that are marked with a specific parameter.
      */
     public static final String TOGGLE_LIST_TYPE = "toggle";
+
+    private static final Set<String> LIST_TYPES = Set.of(TOGGLE_LIST_ITEM, BULLETED_LIST_ITEM, NUMBERED_LIST_ITEM);
+
+    @Override
+    protected void visitListChildren(ObjectNode parentBlock, Deque<Context> contextStack) throws ParseException
+    {
+        Listener listener = contextStack.peek().listener();
+
+        ArrayNode childBlocks = getChildBlocks(parentBlock, CHILDREN);
+
+        // Generally, we want the children to be grouped. Scenarios where this is false are: when there are no
+        // children, there is a single group child, the children are all of a list type.
+        Set<String> types = childBlocks.valueStream()
+            .filter(JsonNode::isObject)
+            .map(node -> node.path(TYPE).asText())
+            .collect(Collectors.toSet());
+        if (childBlocks.isEmpty() || isXWikiGroup(childBlocks.iterator().next()) || (types.size() == 1
+            && LIST_TYPES.contains(types.iterator().next())))
+        {
+            super.visitListChildren(parentBlock, contextStack);
+        } else {
+            listener.beginGroup(Collections.emptyMap());
+            super.visitListChildren(parentBlock, contextStack);
+            listener.endGroup(Collections.emptyMap());
+        }
+    }
+
+    private boolean isXWikiGroup(JsonNode jsonNode)
+    {
+        return jsonNode.isObject() && GROUP.equals(jsonNode.path(TYPE).asText());
+    }
 
     @Override
     protected Map<String, String> getListParameters(ObjectNode listItemBlock)
